d830fcb8f2ee7d8c2a6ffb2eac26350624c17c5b
OptionalLanguor <felmt@hotmail.com>
2017-09-21 13:36:44 -0300

    Corrected errors with classes; Need to implement EntityManager creation of Components.

--- Entities/Entity.hpp
+++ Entities/Entity.hpp
@@ -2,24 +2,26 @@
 #define ENTITY_H
 
 #include "Component.hpp"
-#include <vector>
-#include <glm/glm.hpp>
-using namespace glm;
+//#include <vector>
+//#include <glm/glm.hpp>
+//using namespace glm;
 
 class Entity
 {
 private:
  	static const unsigned int MAX_COMPONENTS = 64;
- 	std::vector<Component> m_components;
+ 	//std::vector<Component> m_components;
  	long long int componentKey;
 
 public:
+	RenderableComponent renderableComp;
 	GLuint id;
 
 	Entity(){
 		componentKey = 0;
 	}
 
+	/*
 	bool addComponent(const int& comp){
 		if(m_components.size() > MAX_COMPONENTS)
 			return false;
@@ -28,6 +30,7 @@ public:
 			componentKey |= comp;
 		}
 	}
+	*/
 };
 
 #endif
